2939b8cdb84aedf50a61f27722906a6c7ebd2194
sillycatproductions <156546562+sillycatproductions@users.noreply.github.com>
2025-03-22 11:57:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,27 @@ fig1 = px.scatter(top10_confirmed,x = top10_confirmed.index, y = 'Confirmed', si
 fig1.write_html('Fig1.html', auto_open = True)
 
 top10_deaths = pd.DataFrame(data.groupby('Country')['Deaths'].sum().nlargest(10).sort_values(ascending = False))
-fig2 = px.bar(top10_deaths, x = 'Deaths', y = top10_deaths.index, height = 600, color = 'Deaths', orientation = 'h', color_continuous_scale = ['deepskyblue','red'], title = 'Top 10 Death Cases Countries')
+fig2 = px.bar(top10_deaths, x = 'Deaths', y = top10_deaths.index, height = 600, color = 'Deaths', orientation = 'h', color_continuous_scale = ['green','red'], title = 'Top 10 Death Cases Countries')
 fig2.write_html('Fig2.html', auto_open = True)
+
+top10_recovered = pd.DataFrame(data.groupby('Country')['Recovered'].sum().nlargest(10).sort_values(ascending = False))
+fig3 = px.scatter(top10_recovered,x = top10_recovered.index, y = 'Recovered', size ='Recovered', size_max = 120, color = top10_recovered.index, title = 'Top 10 Countries with the most Recovered People')
+fig3.write_html('Fig3.html', auto_open = True)
+
+top10_active = pd.DataFrame(data.groupby('Country')['Active'].sum().nlargest(10).sort_values(ascending = False))
+fig4 = px.bar(top10_active, x = 'Active', y = top10_active.index, height = 600, color = 'Active', orientation = 'h', color_continuous_scale = ['green','red'], title = 'Top 10 Active Cases in Countries')
+fig4.write_html('Fig4.html', auto_open = True)
+
+topstates_us = data['Country'] == 'US'
+topstates_us = data[topstates_us].nlargest(5, 'Confirmed')
+
+afig1 = go.Figure(data = [go.Bar(name = 'Confirmed Cases', x = topstates_us['Confirmed'], y = topstates_us['Confirmed'], orientation = 'h')])
+afig1.update_layout(title = 'Most confirmed cases in USA', height = 600)
+afig1.write_html('AFig1.html', auto_open = True)
+
+topstates_uk = data['Country'] == 'United Kingdom'
+topstates_uk = data[topstates_uk].nlargest(5, 'Recovered')
+
+afig2 = go.Figure(data = [go.Bar(name = 'Confirmed Cases', x = topstates_uk['Recovered'], y = topstates_uk['Recovered'], orientation = 'h')])
+afig2.update_layout(title = 'Most recovered cases in United Kingdom', height = 600)
+afig2.write_html('AFig2.html', auto_open = True)
